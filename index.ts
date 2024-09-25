dfd442f783ecfca1dd1f8ee6523404263d5d823f
henrychris <64085210+henrychris@users.noreply.github.com>
2024-09-25 15:07:24 +0100

    complete steps 1,2,3

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+const prompt = "sh> ";
+process.stdout.write(prompt);
+
+for await (let line of console) {
+    line = line.trim();
+
+    if (line === "exit") {
+        process.exit(0);
+    }
+
+    const proc = Bun.spawn({
+        cmd: [line],
+        stdout: "inherit",
+    });
+    await proc.exited;
+ 
+    process.stdout.write(prompt);
+}
